6a3f3136a74cfe74a8edf161b0cc3a7b3c6e6e55
720315104023 <ponmaniarumugam98@gmail.com>
2018-08-30 14:53:45 +0530

    Create remove v.py

---
+++ remove v.py
@@ -0,0 +1,19 @@
+def isAlphabet(x):
+    return x.isalpha()
+ 
+def reverse(string):
+    LIST = toList(string)
+    r = len(LIST) - 1
+    l = 0
+ 
+    while l < r:
+    if not isAlphabet(LIST[l]):
+            l += 1
+        elif not isAlphabet(LIST[r]):
+            r -= 1
+            else:  
+            LIST[l], LIST[r] = swap(LIST[l], LIST[r])
+            l += 1
+            r -= 1
+ 
+    return toString(LIST
