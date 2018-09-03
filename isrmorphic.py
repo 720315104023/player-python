a188f45c09a81188e9357ad7ce5762e15f99e638
720315104023 <ponmaniarumugam98@gmail.com>
2018-09-03 14:34:03 +0530

    Create isrmorphic.py

---
+++ isrmorphic.py
@@ -0,0 +1,17 @@
+def isomorphic(string1, string2):
+	m = len(string1)
+	n = len(string2)
+  if m != n:
+		return "Not isomorphic."
+    marked = [False] * MAX_CHAR:
+    map = [-1] * MAX_CHARS
+    for i in xrange(n):
+    if map[ord(string1[i])] == -1:
+    if marked[ord(string2[i])] == True:
+				return "Not isomorphic."
+        marked[ord(string2[i])] = True
+map[ord(string1[i])] = string2[i]
+elif map[ord(string1[i])] != string2[i]:
+return "Not isomorphic."
+return True
+print isomorphic("1232","2342")
